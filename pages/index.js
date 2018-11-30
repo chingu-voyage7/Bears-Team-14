f1a546dcf6c89c464411d41b94486e3c203262fb
Combiz Salehomoum <combiz.s@gmail.com>
2018-11-30 00:05:18 -0800

    Added sample styled component

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,13 @@
 import React from "react";
-export default () => <div>Hello world!</div>;
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: coral;
+  padding: 0.25rem 1rem;
+  border: solid 2px coral;
+  border-radius: 3px;
+  margin: 0.5rem;
+  font-size: 1rem;
+`;
+
+export default () => <Button>Hello world!</Button>;
